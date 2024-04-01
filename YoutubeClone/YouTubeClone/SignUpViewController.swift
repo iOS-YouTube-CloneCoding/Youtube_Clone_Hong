c58e5e69e39bb90b77e19ddb802f93e1da3c43d7
ujhong7 <ujhong7>
2024-04-01 16:50:07 +0900

    feat: 텍스트필드 모두 채워졌을때 버튼 활성화, 배경색 변경

--- YoutubeClone/YouTubeClone/SignUpViewController.swift
+++ YoutubeClone/YouTubeClone/SignUpViewController.swift
@@ -6,7 +6,25 @@
         imageView.isUserInteractionEnabled = true
         return imageView
     }()
+        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChange(_:)), name: UITextField.textDidChangeNotification, object: nil)
         passwordCheckboxImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(passwordCheckboxTapped)))
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        if isAllFieldsFilled {
+            nextButton.backgroundColor = .systemBlue
+            nextButton.isEnabled = true
+        } else {
+            nextButton.backgroundColor = .gray
+            nextButton.isEnabled = false
+        }
+    }
+    
+    private var isAllFieldsFilled: Bool {
+        return loginInfoStackView.isAllFieldsFilled
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
     @objc func passwordCheckboxTapped() {
         let isChecked = passwordCheckboxImageView.image == UIImage(systemName: "square")
         passwordCheckboxImageView.image = isChecked ? UIImage(systemName: "checkmark.square") : UIImage(systemName: "square")
