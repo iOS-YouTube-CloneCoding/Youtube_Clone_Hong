d973b9db3ea401c477182b09e79e7a18094c413c
ujhong7 <ujhong7>
2024-04-01 16:42:26 +0900

    feat: 텍스트필드 모두 채워졌을때 버튼 활성화, 배경색 변경

--- YoutubeClone/YouTubeClone/LoginViewController.swift
+++ YoutubeClone/YouTubeClone/LoginViewController.swift
@@ -5,3 +5,21 @@
         attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
         label.attributedText = attributedString
         label.textAlignment = .center
+        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChange(_:)), name: UITextField.textDidChangeNotification, object: nil)
+    @objc func textFieldDidChange(_ textField: UITextField) {
+            if isAllFieldsFilled {
+                nextButton.backgroundColor = .systemBlue
+                nextButton.isEnabled = true
+            } else {
+                nextButton.backgroundColor = .gray
+                nextButton.isEnabled = false
+            }
+    }
+    
+    private var isAllFieldsFilled: Bool {
+        return loginInfoStackView.isAllFieldsFilled
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
