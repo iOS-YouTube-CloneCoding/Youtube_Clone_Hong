8dd182e65ad844b8b49d8e1222f46faccaa4f4c1
ujhong7 <ujhong7>
2024-04-01 16:39:51 +0900

    feat: UILabel 행간격 설정

--- YoutubeClone/YouTubeClone/LoginViewController.swift
+++ YoutubeClone/YouTubeClone/LoginViewController.swift
@@ -1,1 +1,7 @@
         label.numberOfLines = 0 // 여러 줄 표시를 위해 설정
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 5
+        let attributedString = NSMutableAttributedString(string: label.text ?? "")
+        attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+        label.attributedText = attributedString
+        label.textAlignment = .center
