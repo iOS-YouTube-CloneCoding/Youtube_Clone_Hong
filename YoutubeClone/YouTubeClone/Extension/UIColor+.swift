20be2b897d920233f96e7894e7a8605922cd3427
ujhong7 <ujhong7>
2024-04-13 11:46:53 +0900

    refactor: 커스텀 컬러 타입 프로퍼티로 변경

--- YoutubeClone/YouTubeClone/Extension/UIColor+.swift
+++ YoutubeClone/YouTubeClone/Extension/UIColor+.swift
@@ -8,11 +8,9 @@
 import UIKit
 
 extension UIColor {
-    /* static func 키워드는 해당 함수가 인스턴스화되지 않고 클래스 이름 자체로 호출될 수 있음을 나타냄.
-     UIColor 클래스의 경우, 색을 나타내는 객체를 만들기 위해 인스턴스화할 필요가 없으므로
-     static 함수를 사용하여 색상을 반환하는 것이 일반적
-     */
-    static func customBlueButtonColor() -> UIColor {
+    static var customBlue: UIColor {
         return UIColor(red: 66/255, green: 133/255, blue: 244/255, alpha: 1.0)
     }
 }
+
+
