9f42d180c27ce0fc4d7f9a27953812b323427348
ujhong7 <ujhong7>
2024-04-30 14:40:11 +0900

    feat: 아이콘 이미지 추가 & 변경

--- YoutubeClone/YouTubeClone/ViewController/TabBar/MainTabController.swift
+++ YoutubeClone/YouTubeClone/ViewController/TabBar/MainTabController.swift
@@ -18,11 +18,11 @@ class MainTabController: UITabBarController {
     private func configureViewControllers() {
         view.backgroundColor = .white
         
-        let home = templateNavigationViewController(title: "홈", unselectedImage: UIImage(systemName: "house")!, selectedImage: UIImage(systemName: "house.fill")!, rootViewController: HomeViewController())
-        let shorts = templateNavigationViewController(title: "Shorts", unselectedImage: UIImage(systemName: "house")!, selectedImage: UIImage(systemName: "house.fill")!, rootViewController: ShortsViewController())
-        let add = templateNavigationViewController(title: "추가", unselectedImage: UIImage(systemName: "house")!, selectedImage: UIImage(systemName: "house.fill")!, rootViewController: AddViewController())
-        let sub = templateNavigationViewController(title: "구독", unselectedImage: UIImage(systemName: "house")!, selectedImage: UIImage(systemName: "house.fill")!, rootViewController: SubscribeViewController())
-        let strg = templateNavigationViewController(title: "삭제", unselectedImage: UIImage(systemName: "house")!, selectedImage: UIImage(systemName: "house.fill")!, rootViewController: StorageViewController())
+        let home = templateNavigationViewController(title: "홈", unselectedImage: UIImage(named: "homeIcon")!, selectedImage: UIImage(named: "homeIconFill")!, rootViewController: HomeViewController())
+        let shorts = templateNavigationViewController(title: "Shorts", unselectedImage: UIImage(named: "shortsIcon")!, selectedImage: UIImage(named: "shortsIconFill")!, rootViewController: ShortsViewController())
+        let add = templateNavigationViewController(title: "추가", unselectedImage: UIImage(named: "plueCircleIcon")!, selectedImage: UIImage(named: "plueCircleIcon")!, rootViewController: AddViewController())
+        let sub = templateNavigationViewController(title: "구독", unselectedImage: UIImage(named: "subscriptionsIcon")!, selectedImage: UIImage(named: "subscriptionsIconFill")!, rootViewController: SubscribeViewController())
+        let strg = templateNavigationViewController(title: "삭제", unselectedImage: UIImage(named: "LibraryIcon")!, selectedImage: UIImage(named: "LibraryIconFill")!, rootViewController: StorageViewController())
                                                     
         viewControllers = [home, shorts, add, sub, strg]
         tabBar.tintColor = .black
