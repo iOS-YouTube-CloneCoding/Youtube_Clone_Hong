ece6cf2cea958d9a4ec1dccdfa650d632b1492e6
ujhong7 <ujhong7>
2024-04-30 14:03:50 +0900

    feat: 탭바에 들어갈 UIViewController 추가

---
+++ YoutubeClone/YouTubeClone/ViewController/TabBar/Tab/SubscribeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SubscribeViewController.swift
+//  YouTubeClone
+//
+//  Created by yujaehong on 4/15/24.
+//
+
+import UIKit
+
+class SubscribeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
