c6d88d519ae116dae137b4be0bb1bcf0735d10a7
ujhong7 <ujhong7>
2024-05-18 15:45:04 +0900

    feat: TabButtonTableViewCell 생성

---
+++ YoutubeClone/YouTubeClone/Scenes/Tab/Subscribe/TableViewCell/TabButtonTableViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  TabButtonTableViewCell.swift
+//  YouTubeSubscribeTab
+//
+//  Created by yujaehong on 5/13/24.
+//
+
+import UIKit
+
+class TabButtonTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let collectionView: TabButtonCollectionView = {
+        let collectionView = TabButtonCollectionView()
+        return collectionView
+    }()
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+        setupAutoLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    private func configureUI() {
+        backgroundColor = .systemBackground
+    }
+    
+    private func setupAutoLayout() {
+        contentView.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        ])
+    }
+}
+
+
+
