5a736b1b7029f1758fd919385f9ffd6432eaaeff
ujhong7 <ujhong7>
2024-05-18 15:10:23 +0900

    feat: 테이블뷰 생성

--- YoutubeClone/YouTubeClone/Scenes/Tab/Subscribe/SubscribeViewController.swift
+++ YoutubeClone/YouTubeClone/Scenes/Tab/Subscribe/SubscribeViewController.swift
@@ -1,3 +1,8 @@
+    private var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
         setupNavigationBarItems()
     private func setupNavigationBarItems() {
         setupLeftNavigationItem()
@@ -16,4 +21,24 @@
         let notificationButton = UIBarButtonItem(image: UIImage(named: "NotificationIcon"), style: .plain, target: self, action: #selector(notificationButtonTapped))
         let searchButton = UIBarButtonItem(image: UIImage(named: "SearchIcon"), style: .plain, target: self, action: #selector(searchButtonTapped))
         navigationItem.rightBarButtonItems = [searchButton, notificationButton, windowSharingButton]
+    }
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(ChannelTableViewCell.self, forCellReuseIdentifier: "ChannelTVC")
+        tableView.register(TabButtonTableViewCell.self, forCellReuseIdentifier: "TabButtonCell")
+        tableView.register(VideoTableViewCell.self, forCellReuseIdentifier: "VideoCell")
+        
+        setupAutoLayout()
+    }
+    
+    private func setupAutoLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
